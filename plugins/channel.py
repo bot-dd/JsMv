4c18d934e0cda22f5cb560bd73630592bc44db65
꯭➺ 🍃⃟AR Rᴀʜᴀᴛ🜲 <premiumbin08@gmail.com>
2025-02-12 20:53:33 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -75,7 +75,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         ]
         selected_poster = poster_url or random.choice(no_poster_images)
 
-        caption_message = f"#New_File_Added ✅\n\n● File Name: <code>{movie_name}</code>\n\n● Language: {language}\n\n● Quality: {quality}"
+        caption_message = f"#New_File_Added ✅\n\n● ꜰɪʟᴇ_ɴᴀᴍᴇ: <code>{movie_name}</code>\n\n● ʟᴀɴɢᴜᴀɢᴇ: {language}\n\n● Qᴜᴀʟɪᴛʏ: {quality}"
         search_movie = movie_name.replace(" ", '-')
         movie_update_channel = await db.movies_update_channel_id()
 
